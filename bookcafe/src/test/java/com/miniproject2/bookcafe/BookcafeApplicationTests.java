037e1eb1c327840057d7ad166b9749c8bbdfbca6
yuseonpark <pyuseon@gmail.com>
2022-02-11 15:59:59 +0900

    프로젝트 생성

---
+++ bookcafe/src/test/java/com/miniproject2/bookcafe/BookcafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miniproject2.bookcafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookcafeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
