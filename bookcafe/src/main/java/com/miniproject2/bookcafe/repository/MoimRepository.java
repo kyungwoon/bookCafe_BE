af0d41097648cb09e663f23a13d5d16790cada51
yuseonpark <pyuseon@gmail.com>
2022-02-12 00:32:26 +0900

    모임 생성, 삭제 / 모임 참가, 참가취소 구현 완료

---
+++ bookcafe/src/main/java/com/miniproject2/bookcafe/repository/MoimRepository.java
@@ -0,0 +1,10 @@
+package com.miniproject2.bookcafe.repository;
+
+import com.miniproject2.bookcafe.domain.Moim;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MoimRepository extends JpaRepository<Moim, Long> {
+    List<Moim> findAllByOrderByModifiedAtDesc();
+}
