b914ff84f69f9529d06309fbf4d956357c506066
yuseonpark <pyuseon@gmail.com>
2022-02-11 16:17:43 +0900

    타임존 설정

--- bookcafe/src/main/java/com/miniproject2/bookcafe/BookcafeApplication.java
+++ bookcafe/src/main/java/com/miniproject2/bookcafe/BookcafeApplication.java
@@ -2,9 +2,19 @@ package com.miniproject2.bookcafe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class BookcafeApplication {
+    @PostConstruct
+    public void started() {
+        // timezone UTC 셋팅
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BookcafeApplication.class, args);
